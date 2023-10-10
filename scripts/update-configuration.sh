4d95a81c124e137c9286845d5e2c783338815fad
Jens Willmer <info@jwillmer.de>
2023-10-10 16:02:45 +0200

    Update update-configuration.sh

--- scripts/update-configuration.sh
+++ scripts/update-configuration.sh
@@ -1,42 +1,100 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# chmod u+x update-configuration.sh
-# ./update-configuration.sh
-#
-# This script makes a copy of the docker template folder.
-# Recursively goes through all files and replaces all variables
-# with the corresponding content from the .env file.
-# And finally copies the folder to the new location.
-#
+# ANSI escape codes for colored text
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
 
-RESTORE_COLOR='\033[0m'
-RED='\033[00;31m'
-GREEN='\033[00;32m'
-YELLOW='\033[00;33m'
-LIGHTGRAY='\033[00;37m'
-CYAN='\033[00;36m'
+# Flag for detailed logging
+DETAILED_LOG=true
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-PARENT=$(dirname $DIR)
+# Function to print logs with colors and optional indentation
+log() {
+    INDENT_LEVEL="$1"
+    COLOR="$2"
+    shift 2
+    MESSAGE="$@"
+    INDENT=$(printf "%${INDENT_LEVEL}s" "")
+    echo -e "${INDENT}${COLOR}${MESSAGE}${NC}"
+}
 
-echo -e "${GREEN}> Writing application settings ${RESTORE_COLOR}"
+# Function to exit script in case of an error
+exit_script() {
+    log 2 $RED "Exiting due to error."
+    exit 1
+}
 
-cp -r $PARENT/templates $PARENT/.config
+# Validate that essential folders and files exist
+validate_environment() {
+    [ ! -d "$TEMPLATE_FOLDER" ] && log 2 $RED "Template folder does not exist." && exit_script
+    [ ! -f "$PARENT_DIR/.env" ] && log 2 $RED ".env file does not exist." && exit_script
+}
 
-# Replace environment variables in all files of the folder
-set -a
-for file in $(find $PARENT/.config -type f)
-do
-    . $PARENT/.env && . $PARENT/.env-secrets && envsubst < $file > $file.tmp && mv $file.tmp $file
-done
-set +a
+# Load environment variables from .env and .env-secrets
+load_environment_variables() {
+    log 2 $GREEN "Loading environment variables..."
+    set -a
+    . "$PARENT_DIR/.env" || log 4 $RED "Could not load .env file."
+    [ -f "$PARENT_DIR/.env-secrets" ] && . "$PARENT_DIR/.env-secrets" || log 4 $RED "Could not load .env-secrets file."
+    set +a
+    if [ "$DETAILED_LOG" = true ]; then
+        log 4 $YELLOW "Loaded environment variables:"
+        env | grep -Eo '^[a-zA-Z0-9_]*=' | sed 's/=$//' | while read -r var; do
+            value=$(eval echo "\$$var")
+            log 6 $YELLOW "$var=$value"
+        done
+    fi
+}
 
-mkdir -p $PARENT/config
 
-# copy if new or modified
-rsync -raz $PARENT/.config/. $PARENT/config/
+# Main function to process templates
+process_templates() {
+    log 0 $GREEN "=== Starting Script ==="
+    validate_environment
+    load_environment_variables
 
-rm -r $PARENT/.config
+    log 2 $GREEN "Starting variable replacement..."
+    mkdir -p "$OUTPUT_FOLDER" || exit_script
 
-echo -e "${GREEN}> Application settings updated${RESTORE_COLOR}"
-echo ""
+    for filepath in $(find "$TEMPLATE_FOLDER" -type f); do
+        relpath="${filepath#$TEMPLATE_FOLDER/}"
+        outfile="$OUTPUT_FOLDER/$relpath"
+        outdir=$(dirname "$outfile")
+
+        mkdir -p "$outdir" || exit_script
+
+        log 4 $YELLOW "Processing file: $relpath"
+
+        # Substitute variables
+        while IFS= read -r line; do
+            echo $line | sed -e "s/\${\([a-zA-Z0-9_]*\)\}/\${\1:-}/g" > temp.txt
+            while IFS= read -r line2; do
+                eval echo "$line2"
+            done < temp.txt
+        done < "$filepath" > "$outfile" || exit_script
+
+        # Debug line after substitution
+        if [ "$DETAILED_LOG" = true ]; then
+            log 6 $YELLOW "After substitution:"
+            while IFS= read -r sub_line; do
+                log 8 $YELLOW "$sub_line"
+            done < "$outfile"
+        fi
+    done
+
+    rm temp.txt || exit_script
+
+    log 0 $GREEN "=== Script Completed ==="
+}
+
+# Get the parent directory of this script
+PARENT_DIR=$(cd "$(dirname "$0")" && pwd -P || exit_script)
+PARENT_DIR=$(dirname "$PARENT_DIR")
+
+# Define the template and output folder paths
+TEMPLATE_FOLDER="$PARENT_DIR/templates"
+OUTPUT_FOLDER="$PARENT_DIR/config"
+
+# Run the function to replace variables in templates and write to output folder
+process_templates
